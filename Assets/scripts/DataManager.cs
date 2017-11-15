b2b11339a1462dc6aac58b40a25a2954a7312a61
pancakepuppy123 <33232427+pancakepuppy123@users.noreply.github.com>
2017-11-15 14:34:15 -0500

    Created the project.

---
+++ Assets/scripts/DataManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour 
+    {
+      public int P1Score = 0;
+      public int P2Score = 0;
+      public int WinningScore = 10;
+      public string WinningMessage = "";
+    }
